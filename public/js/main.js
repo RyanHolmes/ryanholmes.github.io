1561e9b87bc423161e62996af56e891d08d6b9c7
Ryan Holmes <ryankaz94@gmail.com>
2017-03-29 17:04:25 -0400

    some more structure

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,24 @@
 $(document).ready(function(){
-  alert("JQuery Ready");
+  populatePage();
+  $.each($(".gif"), function(){
+    $(this).on("mouseover", function(){
+      $(this).attr("src", "/images/2017-03-29-blob-idle.gif");
+    });
+    $(this).on("mouseleave", function(){
+      $(this).attr("src", "/images/2017-03-29-blob-idle.png");
+    });
+  });
 });
 
-function getGifCard(){
+function populatePage(){
+  $.get("/data", function(data, status){
+    data = JSON.parse(data);
+    $.each(data, function(e){
+      console.log(data[e]);
+    });
+  });
+};
+
+function getGifCard(data){
 
 };
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
 
@@ -6,4 +7,10 @@ app.use(express.static('public'));
 
 app.listen(3000, function(){
   console.log("running on port localhost:3000");
+});
+
+app.get('/data', function(req, res){
+  var data = fs.readFileSync(__dirname + '/data.json');
+  res.send(data);
+  res.sendStatus(200);
 });
